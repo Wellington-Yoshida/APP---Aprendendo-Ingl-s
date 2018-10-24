c99008dd79c7fc86efbe8632e09c38ef76f0010a
Wellington Issami Yoshida <issami-yoshida@hotmail.com>
2018-10-24 07:17:22 -0200

    Processo de tentativa

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,7 @@
+import { Frase } from '../compartilhados/frase.model'
+
+export const FRASES: Frase[] = [
+    { fraseEng: 'Hello Word', frasePtBr: 'Olá Mundo' },
+    { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
+    { fraseEng: 'I like woman', frasePtBr: 'Eu gosto de mulher' }
+]
